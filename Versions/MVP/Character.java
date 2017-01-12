d03095e3d211f73f1066baa1284854c1c8ee002b
EugeneThomas <ethomas1@stuy.edu>
2017-01-11 19:09:21 -0500

    New stuff again

--- Versions/MVP/Character.java
+++ Versions/MVP/Character.java
@@ -4,15 +4,23 @@ public abstract class Character () {
 
 	// Instance Variables: 
 	
+	// The stuff thats currently commented is for the actual version: 
 	protected int numPreds; 
 	protected int numPreys; 
-	// protected double spawn; 
-	// protected String displayStr = ""; 
-	// protected String retStr; 
+	protected double spawn; 
+	protected String displayStr = ""; 
+	protected String retStr; 
+	
+	// MVP Stuff: 
 	protected String name; 
+	
 	
 	// Methods: 
 
+	// Actual Version: 
+	public abstract void editStr(); 
+	
+	// MVP Version: 
 	public abstract String toString(); 
 	
 }
--- Versions/MVP/Predator.java
+++ Versions/MVP/Predator.java
@@ -2,8 +2,27 @@
 
 public class Predator implements Character () { 
 
+	// Instance Variables: 
+    String[] names = new String[3];
+	public int damage; 
+	
+	// Name Array: 
+	names = ["Bonnie the Bear", "Jen the Jaguar", "Allard the Alpaca"];
+	
+	// Default Constructor 
 	public Predator() { 
-		
+		int randInt = (int) (Math.random()*3); 
+		name = names[randInt]; 
+		damage = 10; 
 	}
 
+	public int getDamage () { 
+		return damage; 
+	}
+	
+		
+	// Rewrite this later to reveal info about numPreds and numPrey.
+	public String toString() { 
+		return name; 
+	}
 } 
