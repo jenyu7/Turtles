0beca57b07d0d0924443a8b451dcda5c2e793253
Jen Yu <Jen Yu>
2017-01-18 09:49:27 -0500

    v.1.3 Cazador

---
+++ Versions/v.1.3 Cazador/Animal.java
@@ -0,0 +1,16 @@
+public abstract class Animal { 
+
+	//Declare Variables
+	protected int numPreds; 
+	protected int numPreys; 
+	protected double spawn; 
+	protected String name; 
+	protected String story;
+	protected String susName;
+	
+	// MVP Version: 
+	public abstract String toString(); 
+	
+	public abstract String getSusName();
+	
+}
