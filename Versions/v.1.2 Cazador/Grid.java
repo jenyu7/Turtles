f8f08065feafeffe628730892d0da4f85b030419
jenyu7 <jyu7@stuy.edu>
2017-01-17 22:38:47 -0500

    Modified Grid

--- Versions/v.1.2 Cazador/Grid.java
+++ Versions/v.1.2 Cazador/Grid.java
@@ -24,17 +24,37 @@ public class Grid {
 		initPopulateUsed();
     }
 	
-	//toString() method
+	//Regular toString() method
 	public String toString(){
 		String retStr = "    1     2     3     4     5     6     7     8     9\n";
+		retStr += "  -------------------------------------------------------\n";
 		int count = 1;
 		for (Animal[] a : _grid){
-			retStr += count + "|";
+			retStr += " " + count + "|";
 			for (Animal b: a){
 				if ((b instanceof Prey) || (b instanceof Predator)){retStr += b.getSusName() + "|";}
 				else{retStr += b + "|";}
 			}
 			retStr += "\n";
+			retStr += "  -------------------------------------------------------\n";
+			count ++;
+		}
+		return retStr;
+	}
+	
+	//Overloaded toString() method in order to diagnose errors
+	public String toString(String diag){
+		String retStr = "    1     2     3     4     5     6     7     8     9\n";
+		retStr += "  -------------------------------------------------------\n";
+		int count = 1;
+		for (Animal[] a : _grid){
+			retStr += " " + count + "|";
+			for (Animal b: a){
+				if ((b instanceof Prey) || (b instanceof Predator)){retStr += b.getSusName() + "|";}
+				else{retStr += b + "|";}
+			}
+			retStr += "\n";
+			retStr += "  -------------------------------------------------------\n";
 			count ++;
 		}
 		return retStr;
