425cdaea0b44409a36dd6f8d8042da99765fbd5e
Jen Yu <jyu7@stuy.edu>
2017-01-19 17:41:01 -0500

    Renamed folder and fixed formatting for Woo.java

---
+++ Versions/v.1.4 Cazador/Empty.java
@@ -0,0 +1,25 @@
+public class Empty extends Animal { 
+
+	//Default Constructor (for _usedCors)
+	public Empty() { 
+		name = "empty"; 
+	}
+	
+	//Overloaded Constructor (for populating Grid)
+	public Empty(int xcor, int ycor) { 
+		name = "     "; 
+	}
+	
+	public String toString () { 
+		return name; 
+	}
+	
+	public String getSusName(){
+		return susName;
+	}
+	
+	public void setSusName(String str){
+		susName = str;
+	}
+
+}
